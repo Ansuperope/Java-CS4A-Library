e89b6715fe94fb327dee1b416902f9c4e1fa5af9
Ansuperope <151668459+Ansuperope@users.noreply.github.com>
2026-05-08 17:32:20 -0700

    Initial commit

---
+++ project/Main.java
@@ -0,0 +1,18 @@
+/**
+ * @file Main.java
+ * @brief LOREM
+ * 
+ * @details
+ * LOREM
+ */
+package project;
+
+// Include files from other folders
+import project.include.LOREM;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+}
