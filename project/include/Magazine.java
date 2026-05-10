7778c26e23ee003d2bb138785ecf3f9f8c6263dc
Ansuperope <151668459+Ansuperope@users.noreply.github.com>
2026-05-10 06:49:02 +0000

    some what finished derived classes, will need to look into how to do overrideing output later

--- project/include/Magazine.java
+++ project/include/Magazine.java
@@ -6,6 +6,23 @@
 package project.include;
 import project.include.LibraryItem;
 
-// public class Magazine extends LibraryItem {
+public class Magazine extends LibraryItem {
+    private int edition;
+    private String mainArticle;
+
+    /**
+     * @brief constructor
+     * 
+     * @param ed edition
+     * @param article main article
+     * @param nm name
+     * @param desc description
+     */
+    Magazine (int ed, String article, String nm, String desc) {
+        super(nm, desc);
+        this.edition = ed;
+        this.mainArticle = article;
+    } // END Magazine / constructor
+
     
-// }
+} // END class Magazine
--- project/include/Movie.java
+++ project/include/Movie.java
@@ -4,8 +4,18 @@
  * Derived class
  */
 package project.include;
-// import project.include.LibraryItem;
+import project.include.LibraryItem;
 
-// public class Movie extends LibraryItem {
-    
-// }
+public class Movie extends LibraryItem {
+    private String title;
+    private String director;
+    private String mainActors;
+
+    Movie (String t, String d, String actors, String desc) {
+        super(t, desc);
+        this.title = t;
+        this.director = d;
+        this.mainActors = actors;
+    } // END Movie / constructor
+
+} // END class Movie
